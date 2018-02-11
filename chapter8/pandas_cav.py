7b15d12a7707b936d1439ca140fad8fa27a3aa3a
xiaoxu727 <xuxingyuan727@163.com>
2018-02-11 18:34:28 +0800

    chapter08 version2

--- chapter8/pandas_cav.py
+++ chapter8/pandas_cav.py
@@ -1,1 +1,28 @@
 import pandas as pd
+from pandas import DataFrame, Series
+import numpy as np
+import matplotlib.pyplot as plt
+def line():
+    fig = plt.Figure()
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax2 = fig.add_subplot(2, 2, 2)
+    s = Series(np.random.randn(10).cumsum(), index=np.arange(0, 100, 10))
+    s.plot()
+    plt.show()
+    # print(s)
+def multi_lines():
+
+    df = DataFrame(np.random.randn(10, 4).cumsum(0),
+                   columns=['A', 'B', 'C', 'D'],
+                   index=np.arange(0, 100, 10))
+    plt.show()
+def bar():
+    fig, axes = plt.subplots(2, 1)
+    data = Series(np.random.rand(16), index=list('abcdefghijklmnop'))
+    data.plot(kind='bar', ax=axes[1], color='k', alpha=0.7)
+    data.plot(kind='barh', ax=axes[0], color='k', alpha=0.7)
+    plt.show()
+if __name__ == '__main__':
+    # line()
+    # multi_lines()
+    bar()
